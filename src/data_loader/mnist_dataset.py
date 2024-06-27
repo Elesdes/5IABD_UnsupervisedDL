3acc1c821290e26d7ee87ff46f165fa73c0fc1a2
Elesdes <elesdes404@gmail.com>
2024-06-27 15:45:49 +0200

    Added preprocessing to the load_mnist_dataset

--- src/data_loader/mnist_dataset.py
+++ src/data_loader/mnist_dataset.py
@@ -3,5 +3,10 @@ from keras.datasets import mnist
 import numpy as np
 
 
-def load_mnist_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    return mnist.load_data()
+def load_mnist_data() -> (
+    Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]
+):
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(x_train.shape[0], -1) / 255.0
+    x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
+    return (x_train, y_train), (x_test, y_test)
