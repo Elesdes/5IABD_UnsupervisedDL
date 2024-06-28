5aab9ec1feb16ea50cd500ce6dc3a0ba4956a283
Elesdes <elesdes404@gmail.com>
2024-06-28 04:13:01 +0200

    SOM Completed

--- src/algorithms/som/som.py
+++ src/algorithms/som/som.py
@@ -1,6 +1,6 @@
 import numpy as np
-import pickle
 from tqdm import tqdm
+from src.config.path_config import path_config
 
 
 class SOM:
@@ -15,42 +15,40 @@ class SOM:
         self.neigy = np.arange(y)
         self.decay_function = lambda x, t, max_iter: x / (1 + t / max_iter)
 
-    def __neighborhood_function(self, distance, sigma):
+    def _neighborhood_function(self, distance, sigma):
         return np.exp(-distance / (2 * sigma**2))
 
-    def __calculate_distance(self, x, y, x2, y2):
+    def _calculate_distance(self, x, y, x2, y2):
         return np.sqrt((x - x2) ** 2 + (y - y2) ** 2)
 
-    def __find_bmu(self, sample):
+    def _find_bmu(self, sample):
         distances = np.linalg.norm(self.weights - sample, axis=-1)
-        bmu_idx = np.unravel_index(np.argmin(distances, axis=None), distances.shape)
-        return bmu_idx
+        return np.unravel_index(np.argmin(distances, axis=None), distances.shape)
 
-    def __update_weights(self, sample, bmu_idx, iteration, max_iter):
+    def _update_weights(self, sample, bmu_idx, iteration, max_iter):
         learning_rate = self.decay_function(self.learning_rate, iteration, max_iter)
         sigma = self.decay_function(self.sigma, iteration, max_iter)
 
         for i in range(self.x):
             for j in range(self.y):
-                dist = self.__calculate_distance(i, j, bmu_idx[0], bmu_idx[1])
+                dist = self._calculate_distance(i, j, bmu_idx[0], bmu_idx[1])
                 if dist <= sigma:
-                    influence = self.__neighborhood_function(dist, sigma)
+                    influence = self._neighborhood_function(dist, sigma)
                     self.weights[i, j, :] += (
                         influence * learning_rate * (sample - self.weights[i, j, :])
                     )
 
     def train(self, data, num_iterations):
         for iteration in tqdm(range(num_iterations)):
-            for sample in data:
-                bmu_idx = self.__find_bmu(sample)
-                self.__update_weights(sample, bmu_idx, iteration, num_iterations)
+            sample = data[np.random.randint(0, len(data))]
+            bmu_idx = self._find_bmu(sample)
+            self._update_weights(sample, bmu_idx, iteration, num_iterations)
 
     def map_vects(self, data):
-        mapped = np.array([self.__find_bmu(sample) for sample in data])
-        return mapped
+        return np.array([self._find_bmu(sample) for sample in data])
 
-    def compress(self, file_path):
-        som_state = {
+    def compress(self):
+        return {
             "x": self.x,
             "y": self.y,
             "input_dim": self.input_dim,
@@ -58,13 +56,9 @@ class SOM:
             "sigma": self.sigma,
             "weights": self.weights,
         }
-        with open(file_path, "wb") as f:
-            pickle.dump(som_state, f)
 
     @staticmethod
-    def decompress(file_path):
-        with open(file_path, "rb") as f:
-            som_state = pickle.load(f)
+    def decompress(som_state):
         som = SOM(
             x=som_state["x"],
             y=som_state["y"],
@@ -75,6 +69,6 @@ class SOM:
         som.weights = som_state["weights"]
         return som
 
-    def generate(self, n_samples: int = 100):
+    def generate(self, n_samples):
         indices = np.random.randint(0, self.x * self.y, n_samples)
         return self.weights.reshape(self.x * self.y, self.input_dim)[indices]
